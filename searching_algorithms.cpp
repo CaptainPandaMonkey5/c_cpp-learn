24dfde42bb9354e3c2dfd178b4e55a45046dd0a8
sam <angelcunanan15@gmail.com>
2022-11-23 09:46:16 +0800

    draft

---
+++ searching_algorithms.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int Q[MAX];
+
+inputData();
+getData();
+searchData();
+display();
+
+int main()
+{
+  cout << "------------------------------------------------------" << endl;
+  cout << "           CHAPTER 5: SEARCHING ALGORITHMS            " << endl;
+  cout << "Student Name: Cunanan, Lovely Angeline OL. | BSM CS-2A" << endl;
+  cout << "------------------------------------------------------" << endl;
+  cout << "Please enter Elements: ";
+}
