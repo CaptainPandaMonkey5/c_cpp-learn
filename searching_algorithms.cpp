5f10638f1c1b96ee3f015c64bfd0b207bba9314b
sam <angelcunanan15@gmail.com>
2022-11-26 23:02:25 +0800

    finished

--- searching_algorithms.cpp
+++ searching_algorithms.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int Q[MAX];
+int key;
+int flag, index;
 
-inputData();
-getData();
-searchData();
-display();
+// linear search
+void searchData(int arr[], int size)
+{
+  flag = -1;
+  for (int i = 0; i < size; i++)
+  {
+    if (key == arr[i])
+    {
+      flag = 1;
+      index = i;
+    }
+  }
+}
+
+void display()
+{
+  if (flag == 1)
+  {
+    cout << endl << "Given key " << key << " is positioned at index " << index << " in the array" << endl;
+  }
+  else
+  {
+    cout << endl << "Given key " << key << " is not present in the array" << endl;
+  }
+}
 
 int main()
 {
@@ -16,5 +39,42 @@ int main()
   cout << "           CHAPTER 5: SEARCHING ALGORITHMS            " << endl;
   cout << "Student Name: Cunanan, Lovely Angeline OL. | BSM CS-2A" << endl;
   cout << "------------------------------------------------------" << endl;
-  cout << "Please enter Elements: ";
+  int size;
+    cout << "\tArray Size: ";
+    cin >> size;
+
+  int Q[size];
+  cout << "------------------------------------------------------";
+
+    for (int i = 0; i < size; i++)
+    {
+      cout << "\n\tInput the element #" << i + 1 << ": ";
+      cin >> Q[i];
+    }
+
+    // from the library <bits/stdc++.h>
+    int n = sizeof(Q) / sizeof(Q[0]);
+    sort(Q, Q + n);
+
+  cout << "------------------------------------------------------";
+
+    cout << "\n\tKey: ";
+    cin >> key;
+
+  cout << "------------------------------------------------------";
+
+      cout << "\n\tSorted: ";
+      for (int i = 0; i < size; i++)
+      {
+        cout << Q[i] << ", ";
+      }
+      cout << endl;
+
+      cout << "\n\tKey: " << key << endl;
+
+  searchData(Q, size);
+  display();
+  cout << "------------------------------------------------------" << endl;
+
+  return 0;
 }
